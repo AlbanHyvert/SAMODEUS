f47db59217087060cccfab9891a4a31c9b2f3c1d
A.Hyvert <alban.hyvert@gmail.com>
2020-02-25 11:33:58 +0100

    [SCRIPT] scripts Rotating obj

---
+++ Assets/Scripts/Controller/WorldScripts/GCFRotatingCube.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class GCFRotatingCube : MonoBehaviour
+{
+    private float _rotatingSpeed = 0;
+    private float _distFromStartingPoint = 0f;
+    private float _tempDistFromStartPosition = 0f;
+    private Vector3 _startPosition = Vector3.zero;
+    private Vector3 _playerPosition = Vector3.zero;
+    private float _timePass = 0f;
+    private float _timeResetPos = 0f;
+    private float _returnSpeedPosition = 2f;
+    private void Start()
+    {
+        _startPosition = transform.position;
+        _rotatingSpeed = GCFManager.Instance.Speed;
+        _distFromStartingPoint = (int)Random.Range(GCFManager.Instance.MinAmplitude, GCFManager.Instance.MaxAmplitude);
+        _tempDistFromStartPosition = _distFromStartingPoint;
+    }
+
+    private void LateUpdate()
+    {
+        _playerPosition = PlayerManager.Instance.Player.transform.position;
+
+        _timePass += Time.deltaTime;
+
+        float OffsetDiffFromPlayer = Vector3.Distance(_startPosition, _playerPosition);
+
+        if (OffsetDiffFromPlayer <= GCFManager.Instance.ActionRayon)
+        {
+            _timeResetPos = 0;
+            _timeResetPos += Time.deltaTime;
+            _distFromStartingPoint = Mathf.Lerp(_distFromStartingPoint, 0, _timeResetPos * _returnSpeedPosition);
+        }
+        else
+        {
+            _timeResetPos = 0;
+            _timeResetPos += Time.deltaTime;
+            if (_distFromStartingPoint < _tempDistFromStartPosition)
+            {
+                _distFromStartingPoint = Mathf.Lerp(_distFromStartingPoint, _tempDistFromStartPosition, _timeResetPos * _returnSpeedPosition);
+            }
+        }
+
+        transform.position = _startPosition + Vector3.forward * Mathf.Sin(_timePass * _rotatingSpeed) * _distFromStartingPoint;
+    }
+}
--- Assets/Scripts/Manager/GCFManager.cs
+++ Assets/Scripts/Manager/GCFManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 public class GCFManager : Singleton<GCFManager>
 {
     [SerializeField, Range(0, 4)] private float _speed = 0.2f;
+    [SerializeField, Range(0, 4)] private float _returnSpeed = 0.5f;
     [SerializeField] private int _speedMult = 2;
     [SerializeField] private float _speedMax = 1f;
     [SerializeField] private float _actionRayon = 5f;
     [SerializeField] private float _maxAmplitude = 100;
     [SerializeField] private float _minAmplitude = 10;
     public float Speed { get { return _speed; } }
+    public float ReturnSpeed { get { return _returnSpeed; } }
     public int SpeedMult { get { return _speedMult; } }
     public float SpeedMax { get { return _speedMax; } }
     public float ActionRayon { get { return _actionRayon; } }
