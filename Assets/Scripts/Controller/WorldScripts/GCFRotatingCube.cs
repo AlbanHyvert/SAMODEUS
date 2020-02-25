405af65ffbb9ee5cf8be5171df6e0a8c79f3c67b
A.Hyvert <alban.hyvert@gmail.com>
2020-02-25 15:57:47 +0100

    [SCRIPTS] Fix and update of the GameScene

--- Assets/Scripts/Controller/WorldScripts/GCFRotatingCube.cs
+++ Assets/Scripts/Controller/WorldScripts/GCFRotatingCube.cs
@@ -2,45 +2,37 @@
 
 public class GCFRotatingCube : MonoBehaviour
 {
-    [SerializeField] private Transform _orbitTarget = null;
-    private Quaternion _startRotation = Quaternion.identity;
-    private float _rotatingSpeed = 0;
     private float _distFromStartingPoint = 0f;
-    private float _tempDistFromStartPosition = 0f;
+    private float _distMin = 0f;
+    private float _distMax = 0f;
     private Vector3 _startPosition = Vector3.zero;
+    private Quaternion _startRotation = Quaternion.identity;
     private Vector3 _playerPosition = Vector3.zero;
-    private float _timePass = 0f;
-    private float _timeResetPos = 0f;
-    private float _returnSpeedPosition = 2f;
     private void Start()
     {
         _startPosition = transform.position;
         _startRotation = transform.rotation;
-        _rotatingSpeed = GCFManager.Instance.Speed;
-        _distFromStartingPoint = (int)Random.Range(GCFManager.Instance.MinAmplitude, GCFManager.Instance.MaxAmplitude);
-        _tempDistFromStartPosition = _distFromStartingPoint;
     }
 
-    private void LateUpdate()
+    public void Init(float speedMin, float speedMax)
     {
-        _playerPosition = PlayerManager.Instance.Player.transform.position;
+        _distMin = speedMin;
+        _distMax = speedMax;
+        _distFromStartingPoint = Random.Range(_distMin, _distMax);
+    }
 
-        _timePass += Time.deltaTime;
-
-        float OffsetDiffFromPlayer = Vector3.Distance(_startPosition, _playerPosition);
+    public void Orbit(Vector3 playerPosition, Transform target, Vector3 orientation)
+    {
+        float OffsetDiffFromPlayer = Vector3.Distance(_startPosition, playerPosition);
 
         if (OffsetDiffFromPlayer <= GCFManager.Instance.ActionRayon)
         {
-            _timeResetPos = 0;
-            _timeResetPos += Time.deltaTime;
             transform.position = _startPosition;
         }
         else
         {
-            transform.RotateAround(_orbitTarget.position, Vector3.forward, _distFromStartingPoint * Time.deltaTime);
+            transform.RotateAround(target.position, orientation, _distFromStartingPoint * Time.deltaTime);
             transform.rotation = _startRotation;
         }
-
-
     }
 }
