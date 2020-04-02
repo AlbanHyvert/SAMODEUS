7accee9a16bd5e3ba69632a8cbf0614fe482d47d
AlbanHyvert <40693949+AlbanHyvert@users.noreply.github.com>
2020-04-02 15:38:46 +0200

    [SCRIPT] fix on the disolve shader + teleporter

--- Assets/Scripts/Environmentals/CheckPoints/DestroyPortals.cs
+++ Assets/Scripts/Environmentals/CheckPoints/DestroyPortals.cs
@@ -5,13 +5,29 @@ public class DestroyPortals : MonoBehaviour
 {
     [SerializeField] private Portal[] _portals = null;
     [SerializeField] private GameObject[] _plane = null;
-    [SerializeField] private Renderer[] _renderer = null;
+    [SerializeField] private GameObject[] _gameObject = null;
+
+    [SerializeField] private float _speedDisolve = 1;
+    [SerializeField] private float _maximalTimerValue = 10;
+
+    private bool _isActive = false;
+    private float _timer = 0f;
+
+    private void Start()
+    {
+        GameLoopManager.Instance.Puzzles += OnUpdate;
+        _timer = 0.0f;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         PlayerController player = other.GetComponent<PlayerController>();
 
-        if(player != null)
+        if (player != null && _isActive == false)
         {
+            _timer = 0.0f;
+            _isActive = true;
+
             if(_portals != null)
             {
                 for (int i = 0; i < _portals.Length; i++)
@@ -27,19 +43,40 @@ public class DestroyPortals : MonoBehaviour
                     Object.Destroy(_plane[i], 1);
                 }
             }
+        }
+    }
 
-            if(_renderer != null)
+    private void OnUpdate()
+    {
+        if(_isActive == true)
+        {
+            _timer += 0.01f * (_speedDisolve* Time.deltaTime);
+
+            if (_gameObject != null)
             {
-                for (int i = 0; i < _renderer.Length; i++)
+                for (int i = 0; i < _gameObject.Length; i++)
                 {
-                    GameObject obj = _renderer[i].GetComponent<GameObject>();
-
-                    if(obj != null)
+                    if (_gameObject[i] != null)
                     {
-                        obj.SetActive(false);
+                        Renderer renderer = _gameObject[i].GetComponent<Renderer>();
+
+                        if (renderer != null)
+                            renderer.material.SetFloat("Vector1_3996BBE4", _timer);
                     }
                 }
             }
+
+            if(_timer >= _maximalTimerValue)
+            {
+                for (int i = 0; i < _gameObject.Length; i++)
+                {
+                    Object.Destroy(_gameObject[i]);
+                }
+            }
+        }
+        else
+        {
+            _timer = 0.0f;
         }
     }
 }
