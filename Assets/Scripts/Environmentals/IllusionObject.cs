4d37ed366f812d061518221ce7ade67e20f7002e
AlbanHyvert <40693949+AlbanHyvert@users.noreply.github.com>
2020-04-07 16:28:43 +0200

    [SCRIPT] update illusionObject collider enabled or not from the distance with player

--- Assets/Scripts/Environmentals/IllusionObject.cs
+++ Assets/Scripts/Environmentals/IllusionObject.cs
@@ -4,6 +4,7 @@ public class IllusionObject : MonoBehaviour
 {
     [SerializeField] private Renderer _renderer = null;
     [SerializeField] private bool _isSpecialEvent = false;
+    [SerializeField, Range(0.01f, 0.9f)] private float _valueBeforeDesactivate = 0.3f;
 
     private float _alphaValue = 0.0f;
     private float _maxValue = 1f;
@@ -31,18 +32,35 @@ public class IllusionObject : MonoBehaviour
                 _alphaValue -= 0.1f * Time.deltaTime;
             }
         }
+        else
+        {
+            float _distanceFromPlayer = Vector3.Distance(PlayerManager.Instance.Player.transform.position, transform.position);
 
-        float _distanceFromPlayer = Vector3.Distance(PlayerManager.Instance.Player.transform.position, transform.position);
+            if (_distanceFromPlayer <= 100)
+            {
+                _alphaValue = _distanceFromPlayer / 100;
+            }
+        }
 
         Material material = _renderer.material;
-
-        if(_isSpecialEvent == false && _distanceFromPlayer <= 100)
-        {
-            _alphaValue = _distanceFromPlayer / 100;
-        }
 
         material.SetColor("_BaseColor", (new Color(1, 1, 1, _alphaValue)));
 
         _renderer.material = material;
+
+        if(_alphaValue <= _valueBeforeDesactivate)
+        {
+            bool value = transform.GetComponent<Collider>().enabled;
+
+            if(value != false)
+                transform.GetComponent<Collider>().enabled = !value;
+        }
+        else
+        {
+            bool value = transform.GetComponent<Collider>().enabled;
+
+            if (value != true)
+                transform.GetComponent<Collider>().enabled = !value;
+        }
     }
 }
