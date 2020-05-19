c3b5cf871dde803b0e768bb73fcfdb136c4a35f8
AlbanHyvert <40693949+AlbanHyvert@users.noreply.github.com>
2020-05-19 11:29:56 +0200

    [SCRIPTS] Room4 script done functionnality OK

---
+++ Assets/Scripts/Environmentals/Puzzles/CreateDeathCube.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDeathCube : MonoBehaviour
+{
+    [SerializeField] private Transform[] _distanceBeforeDesctruction = null;
+    [SerializeField] private Transform[] _spawnersPosition = null;
+    [SerializeField] private MovingDeathCube _deathCube = null;
+    [SerializeField] private float _timeBeforeSpawn = 3;
+    [SerializeField] private float _deathCubeSpeed = 3;
+
+    private float _timer = 0;
+    private int _i = 0;
+    private List<MovingDeathCube> _deathCubeList = null;
+
+    private void Start()
+    {
+        _deathCubeList = new List<MovingDeathCube>();
+        GameLoopManager.Instance.Puzzles += OnUpdate;
+        GameLoopManager.Instance.Pause += IsPaused;
+        _timer = Time.time + _timeBeforeSpawn;
+    }
+
+    private void IsPaused(bool value)
+    {
+        if (value == true)
+        {
+            GameLoopManager.Instance.Puzzles -= OnUpdate;
+        }
+        else
+        {
+            GameLoopManager.Instance.Puzzles += OnUpdate;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController playerController = other.GetComponent<PlayerController>();
+
+        if(playerController != null)
+        {
+            GameLoopManager.Instance.Puzzles -= OnUpdate;
+            GameLoopManager.Instance.Pause -= IsPaused;
+
+            for (int i = 0; i < _deathCubeList.Count; i++)
+            {
+                if(_deathCubeList[i] != null)
+                {
+                    Object.Destroy(_deathCubeList[i].gameObject);
+                }
+            }
+
+            _deathCubeList.Clear();
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnUpdate()
+    {
+        if(Time.time > _timer)
+        {
+            if(_i < _spawnersPosition.Length)
+            {
+                _timer = Time.time + _timeBeforeSpawn;
+                MovingDeathCube deathCube = Instantiate(_deathCube, _spawnersPosition[_i].position, Quaternion.identity);
+                deathCube.DistanceBeforeDesctruction = _distanceBeforeDesctruction[_i];
+                deathCube.Speed = _deathCubeSpeed;
+                _i += 1;
+                _deathCubeList.Add(deathCube);
+            }
+            else
+            {
+                _i = 0;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        GameLoopManager.Instance.Puzzles -= OnUpdate;
+        GameLoopManager.Instance.Pause -= IsPaused;
+    }
+}
---
+++ Assets/Scripts/Environmentals/Puzzles/MovingDeathCube.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class MovingDeathCube : MonoBehaviour
+{
+    [SerializeField] private Transform _distanceBeforeDesctruction;
+    [SerializeField] private float _speed = 3;
+
+    private float _timer = 0;
+
+    public Transform DistanceBeforeDesctruction { get => _distanceBeforeDesctruction; set => _distanceBeforeDesctruction = value; }
+    public float Speed { get => _speed; set => _speed = value; }
+
+    private void Start()
+    {
+        GameLoopManager.Instance.Puzzles += OnUpdate;
+        GameLoopManager.Instance.Pause += IsPaused;
+    }
+
+    private void IsPaused(bool value)
+    {
+        if(value == true)
+        {
+            GameLoopManager.Instance.Puzzles -= OnUpdate;
+        }
+        else
+        {
+            GameLoopManager.Instance.Puzzles += OnUpdate;
+        }
+    }
+
+    private void OnUpdate()
+    {
+        float distBeforeDesctruct = Vector3.Distance(transform.position, _distanceBeforeDesctruction.position);
+
+        if( distBeforeDesctruct > 2)
+        {
+            _timer = _speed * Time.deltaTime;
+
+            transform.position = Vector3.Lerp(transform.position, _distanceBeforeDesctruction.position, _timer);
+        }
+        else
+        {
+            Object.Destroy(gameObject);
+            Debug.Log("Destroyed");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        GameLoopManager.Instance.Puzzles -= OnUpdate;
+        GameLoopManager.Instance.Pause -= IsPaused;
+    }
+}
