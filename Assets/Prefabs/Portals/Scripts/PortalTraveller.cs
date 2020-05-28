c5b1ae41dfc3f282b99b5e6482d0c29779c6b63a
AlexandreLeboube <z0st@hotmail.fr>
2020-05-28 16:22:55 +0200

    [FIX] add tout

---
+++ Assets/Prefabs/Portals/Scripts/PortalTraveller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PortalTraveller : MonoBehaviour
+{
+    public Vector3 PreviousOffsetFromPortal { get; set; }
+
+    public virtual void Teleport(Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot, bool shakeValue)
+    {
+        transform.position = pos;
+        transform.rotation = rot;
+        PlayerManager.Instance.Player.CameraController.ShouldShake = shakeValue;
+    }
+
+    public virtual void EnterPortalThreshold()
+    {
+
+    }
+
+    public virtual void ExitPortalThreshold()
+    {
+
+    }
+}
