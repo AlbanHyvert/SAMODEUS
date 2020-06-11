20fd594a1f50b15874a2fb2b41ef53383fec34ef
AlbanHyvert <40693949+AlbanHyvert@users.noreply.github.com>
2020-06-11 12:00:40 +0200

    [POLISH] add resizing effect for the movingDeathCube for more plaising effect

--- Assets/Scripts/Environmentals/Puzzles/MovingDeathCube.cs
+++ Assets/Scripts/Environmentals/Puzzles/MovingDeathCube.cs
@@ -4,14 +4,19 @@ public class MovingDeathCube : MonoBehaviour
 {
     [SerializeField] private Transform _distanceBeforeDesctruction = null;
     [SerializeField] private float _speed = 3;
-
+    
+    private float _distanceBeforeNoEffet = 0.4f;
     private float _timer = 0;
+    private Vector3 _tempScale = Vector3.zero;
+    private float _rescaleTime = 0;
 
     public Transform DistanceBeforeDesctruction { get => _distanceBeforeDesctruction; set => _distanceBeforeDesctruction = value; }
     public float Speed { get => _speed; set => _speed = value; }
 
     private void Start()
     {
+        _tempScale = transform.localScale;
+        transform.localScale = Vector3.zero;
         GameLoopManager.Instance.Puzzles += OnUpdate;
         GameLoopManager.Instance.Pause += IsPaused;
         _timer = 0;
@@ -33,15 +38,30 @@ public class MovingDeathCube : MonoBehaviour
     {
         float distBeforeDesctruct = Vector3.Distance(transform.position, _distanceBeforeDesctruction.position);
 
-        _timer += _speed;
-
-        if ( distBeforeDesctruct > 2)
+        if ( distBeforeDesctruct > 0.1f)
         {
-            transform.position = Vector3.Lerp(transform.position, _distanceBeforeDesctruction.position, _timer * Time.deltaTime);
+            _timer = _speed * Time.deltaTime;
+            _rescaleTime = Time.deltaTime;
+
+            transform.localScale = Vector3.Lerp(transform.localScale, _tempScale, _rescaleTime);
+            transform.position = Vector3.Lerp(transform.position, _distanceBeforeDesctruction.position, _timer);
         }
         else
         {
-            Object.Destroy(gameObject);
+            if(transform.localScale != Vector3.zero)
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, _speed * Time.deltaTime);
+                float distfromOriginalSize = Vector3.Distance(transform.localScale, _tempScale / 2);
+                if (distfromOriginalSize <= _distanceBeforeNoEffet)
+                {
+                    Collider collider = transform.GetComponent<Collider>();
+                    collider.enabled = false;
+                }
+            }
+            else
+            {
+                Object.Destroy(gameObject);
+            }   
         }
     }
 
